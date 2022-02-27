2bcd56906abd5553f1e1e6cd2ae7f6f7bc466c3f
butter-free <butterfree@kakao.com>
2022-02-28 00:51:06 +0900

    feat, docs: constant 파일 추가 및 types 파일 내용 변경
    union type으로 지정된 type을 리스트로 활용을 위해 constant에 배열 생성 후 typeof 키워드로 타입 지정

---
+++ src/common/constant.ts
@@ -0,0 +1,2 @@
+export const LOCATION = ["온라인", "오프라인"]
+export const CATEGORY = ["인문", "금융", "자기계발", "기술", "예술", "철학", "문학"]
--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,8 @@
+import { LOCATION, CATEGORY } from "./constant";
+
+export type Location = typeof LOCATION[number]
+export type Category = typeof CATEGORY[number]
+
 export type Nullable<Element> = Element | null;
 export type Undefinable<Element> = Element | undefined;
 export type Optional<Element> = Nullable<Element> | Undefinable<Element>
