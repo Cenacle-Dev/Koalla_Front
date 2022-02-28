f2c31fd741606ff2f57c397d89d28dbae86b84fa
butter-free <butterfree@kakao.com>
2022-02-28 22:36:16 +0900

    feat, style: imageUpload 컴포넌트 리사이징 기능 추가 및 css 변경
     - 추후 용량 제한 등의 문제시 고려를 위해 이미지 업로드시 리사이징 기능 추가
     - 이미지 프리뷰 시 비율 1:1 처리를 위해 img aspect-ratio: 1 추가
     - 혼동될수 있는 메서드명 변경 및 프리뷰 이미지 분기조건 추가

--- src/components/common/imageUpload.tsx
+++ src/components/common/imageUpload.tsx
@@ -1,22 +1,37 @@
 import React, { Fragment, useEffect, useState } from "react";
 
+import Resizer from "react-image-file-resizer"
+
 import styled from "styled-components";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { red } from "@mui/material/colors";
 
 import { Optional } from "../../common/types";
 
+/**
+ * 이미지 리사이징 크기설정
+ */
+type ImageSize = {
+  readonly maxHeight: number;
+  readonly maxWidth: number;
+}
+
 export interface ImageUploadProps {
-  onSelectedFile: (file: File) => void;
+  readonly imageSize?: ImageSize;
+  onChangeFile: (file: File) => void;
 }
 
 const ImageInput = styled.input``;
 const Image = styled.img`
   height: 100%;
   width: 100%;
+  aspect-ratio: 1;
 `;
 
-export const ImageUpload: React.FC<ImageUploadProps> = ({ onSelectedFile }) => {
+/**
+ * 이미지 업로드시 프리뷰 및 파일처리
+ */
+export const ImageUpload: React.FC<ImageUploadProps> = ({ imageSize, onChangeFile }) => {
   const [selectedFile, setSelectedFile] = useState<File>();
   const [previewURL, setPreviewURL] = useState<string>();
 
@@ -33,7 +48,7 @@ export const ImageUpload: React.FC<ImageUploadProps> = ({ onSelectedFile }) => {
     return () => URL.revokeObjectURL(objectUrl)
   }, [selectedFile]);
 
-  const onSelectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const fileChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files || event.target.files.length === 0) {
       setSelectedFile(undefined);
       return;
@@ -42,8 +57,29 @@ export const ImageUpload: React.FC<ImageUploadProps> = ({ onSelectedFile }) => {
     const imageFile: Optional<File> = event.target.files[0];
 
     if (imageFile) {
+
+      if (imageSize) {
+        try {
+          Resizer.imageFileResizer(
+            imageFile,
+            imageSize?.maxWidth ?? 0,
+            imageSize?.maxHeight ?? 0,
+            "JPEG",
+            100,
+            0,
+            (url) => {
+              const stringURL = url as string
+              console.log(stringURL);
+              setPreviewURL(stringURL);
+            },
+          )
+        } catch (error) {
+          console.log(error);
+        }
+      } 
+
       setSelectedFile(imageFile);
-      onSelectedFile(imageFile);
+      onChangeFile(imageFile);
     } else {
       setSelectedFile(undefined);
     }
@@ -68,12 +104,12 @@ export const ImageUpload: React.FC<ImageUploadProps> = ({ onSelectedFile }) => {
           <ImageInput
             id="imageInputId"
             type="file"
-            onChange={onSelectFile}
+            onChange={fileChangeHandler}
             hidden
           />
         </Fragment>
       )}
-      {selectedFile && <Image src={previewURL} />}
+      {selectedFile && previewURL && <Image src={previewURL} />}
     </div>
   );
 };
