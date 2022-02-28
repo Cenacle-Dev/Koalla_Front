e844e29911ca15abfb92c84964b2384702d20c9a
hamelln <vusdus91@gmail.com>
2022-02-28 16:58:57 +0900

    feat: 모달창 여는 버튼 추가.

---
+++ src/components/addMeeting/styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const OpenModal = styled.button`
+	background-color: transparent;
+	cursor: pointer;
+	position: fixed;
+	bottom: 60px;
+	right: 60px;
+`;
+
+export { OpenModal };
