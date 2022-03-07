cc19e5100a9a7c551cfdebbbabb34122859ef7f7
hamelln <vusdus91@gmail.com>
2022-03-07 15:30:01 +0900

    style: 스타일 수정

--- src/components/meeting_detail/styles.tsx
+++ src/components/meeting_detail/styles.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Main = styled.main`
   background-color: #fdfdfd;
@@ -48,6 +48,8 @@ const NumberOfMembers = styled.div`
   bottom: 18px;
   right: 14px;
 `;
+
+// ? 모임 소개
 const Intro = styled.div`
   font-size: 18px;
   display: flex;
@@ -57,7 +59,8 @@ const Intro = styled.div`
   @media (max-width: 768px) {
     flex-direction: column;
     align-items: flex-end;
-    gap: 4px;
+    gap: 8px;
+    margin: 8px 8px 10px 0;
   }
 `;
 const Figcaption = styled.figcaption`
@@ -67,7 +70,7 @@ const Figcaption = styled.figcaption`
   @media (min-width: 768px) {
     font-size: 13px;
   }
-  @media (min-width: 768px) {
+  @media (min-width: 1024px) {
     font-size: 16px;
   }
 `;
@@ -102,69 +105,133 @@ const Description = styled.div`
   padding: 4px 8px;
   text-align: left;
 `;
-const CurrentMeeting = styled.div`
+
+// ? 공통
+
+const Section = css`
   position: relative;
+  @media (min-width: 640px) {
+    text-align: left;
+  }
+`;
+
+const Frame = css`
+  width: 230px;
+  height: 300px;
+  margin-right: 20px;
+`;
+
+// ? 진행중인 책, 모임
+
+const CurrentMeeting = styled.section`
+  display: grid;
+  grid-template-columns: 230px 180px 180px 180px;
+  grid-template-rows: ;
 `;
 const CurrentContainer = styled.div`
   display: flex;
   @media (max-width: 640px) {
     flex-direction: column;
     align-items: center;
+    gap: 10px;
   }
 `;
+
 const Book = styled.div`
-  width: 200px;
-  height: 240px;
-  margin-right: 20px;
+  ${Frame};
   box-shadow: 5px 5px 10px #6d6d6d45;
   @media (max-width: 640px) {
     margin: 0;
   }
 `;
+
 const BookCover = styled.img`
-  width: 200px;
-  height: 240px;
-  border-radius: 1px;
+  width: 230px;
+  height: 300px;
 `;
-const Members = styled.div`
+const Members = styled.ul`
   display: flex;
   flex-wrap: wrap;
+  align-items: flex-start;
   @media (max-width: 640px) {
     margin-top: 10px;
-    flex-direction: column;
-    align-items: flex-end;
     gap: 10px;
   }
+  & :nth-child(2n) {
+    justify-content: start;
+  }
+  & :nth-child(2n + 1) {
+    justify-content: end;
+  }
 `;
-const Member = styled.div`
+const Member = styled.li`
   display: flex;
   align-items: center;
   gap: 10px;
   flex: 0 0 50%;
+  @media (max-width: 640px) {
+    font-size: 12px;
+    flex: 1 1 34%;
+    gap: 4px;
+    margin-bottom: 6px;
+  }
 `;
 const Profile = styled.img`
   width: 60px;
   height: 60px;
   border-radius: 50%;
+  @media (max-width: 640px) {
+    width: 40px;
+    height: 40px;
+    justify-content: ;
+  }
 `;
-const AfterBooks = styled.div`
-  text-align: left;
+
+// ? 진행 예정 도서
+
+const AfterBooks = styled.section`
+  ${Section};
 `;
+const AfterBook = styled.li`
+  ${Frame};
+  @media (max-width: 768px) {
+    flex: 0 0 33.3%;
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
 const BooksContainer = styled.ul`
   display: flex;
+  flex-wrap: wrap;
 `;
-const MeetingNotes = styled.div`
-  text-align: left;
+
+// ? 모임 노트
+
+const MeetingNotes = styled.section`
+  ${Section};
 `;
 const NotesContainer = styled.ul`
   display: flex;
   gap: 20px;
+  @media (max-width: 768px) {
+    gap: 0;
+  }
 `;
-const Note = styled.div`
+const Note = styled.li`
   position: relative;
   display: flex;
   justify-content: center;
+  @media (max-width: 768px) {
+    flex: 1 1 33.3%;
+  }
 `;
+
+const NoteCover = styled.img`
+  width: 100%;
+`;
+
 const NoteUser = styled.h3`
   position: absolute;
   bottom: 2px;
@@ -198,4 +265,6 @@ export {
   NotesContainer,
   NoteUser,
   Buttons,
+  AfterBook,
+  NoteCover,
 };
